14e78e9f709e6cfc94cb3310006105814ebd5761
Saul Augusto Gasca Farrera <sgfarreradev@gmail.com>
2024-04-18 12:13:11 -0600

    Project Beta v4

--- src/app/(main)/summarize/page.tsx
+++ src/app/(main)/summarize/page.tsx
@@ -11,25 +11,25 @@ const SummarizePage = () => {
 
  
 
-  let botResponses = localStorage.getItem("savedResponses")
+  //let botResponses = localStorage.getItem("savedResponses")
 
   let summarizeContent = ""
 
   useEffect(() => {
 
    
-        // Verificar si estamos en el entorno del cliente
-        if (typeof window !== 'undefined') {
-          const botResponses = localStorage.getItem('savedResponses');
-          if (botResponses) {
-            const savedResponsesArray = JSON.parse(botResponses);
-            const contentArray = savedResponsesArray.map((response: any) => response.content);
-            setResponses(contentArray);
-          }
-        }
+       // Verificar si estamos en el entorno del cliente
+    if (typeof window !== 'undefined') {
+      const botResponses = localStorage.getItem('savedResponses');
+      if (botResponses) {
+        const savedResponsesArray = JSON.parse(botResponses);
+        const contentArray = savedResponsesArray.map((response: any) => response.content);
+        setResponses(contentArray);
+      }
+    }
     
 
-  },[botResponses])
+  },[])
 
 
   const saveSummarizeToLocalStorage = () => {
